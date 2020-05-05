53223b9e01a82f844ba5ca4b9697178c25f09dea
vtiron2002 <vtiron2002@gmail.com>
2020-05-04 22:39:36 -0400

    Updates

---
+++ precache-manifest.65af6cab4ffd0eb5d865e9b148c54746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4393ea2f4fbe4e387d239e0f9690fb8",
+    "url": "/login-page/index.html"
+  },
+  {
+    "revision": "bd074bb7aa69057dd006",
+    "url": "/login-page/static/css/main.3661cdfd.chunk.css"
+  },
+  {
+    "revision": "45d6575cea3f959fe327",
+    "url": "/login-page/static/js/2.e989d0dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login-page/static/js/2.e989d0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd074bb7aa69057dd006",
+    "url": "/login-page/static/js/main.5b53e456.chunk.js"
+  },
+  {
+    "revision": "8cfd6c460e8a07a36d6e",
+    "url": "/login-page/static/js/runtime-main.fece62ec.js"
+  },
+  {
+    "revision": "47c6283601212967d32182d72dfe5f3c",
+    "url": "/login-page/static/media/leftPic.47c62836.jpeg"
+  }
+]);
